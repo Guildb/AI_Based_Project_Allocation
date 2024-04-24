937d16202c31998c34539d9fc09be71f468ecb5f
guildb <5silvr53@solent.ac.uk>
2024-04-24 23:48:13 +0100

    working on nav bar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,15 +5,6 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <router-link
-        to="/dashboard"
-        class="flex items-center space-x-3 rtl:space-x-reverse"
-      >
-        <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >Project Allocation</span
-        >
-      </router-link>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button
           @click="toggleMenu"
