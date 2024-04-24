1072277016bfca83e54aef8086b167edfeb70018
guildb <5silvr53@solent.ac.uk>
2024-04-24 23:46:17 +0100

    reverse changes as it breeack the menu

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -18,7 +18,7 @@
         <button
           @click="toggleMenu"
           type="button"
-          class="toggle-button inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky"
           aria-expanded="false"
         >
@@ -189,15 +189,5 @@ export default {
 }
 nav {
   animation: slideDown 0.5s ease-out forwards;
-}
-@media (max-width: 849px) {
-  #navbar-sticky {
-    display: none; /* Hide by default when under 850px */
-  }
-}
-@media (max-width: 849px) {
-  .toggle-button {
-    display: block !important; /* Important to override any other hidden classes */
-  }
 }
 </style>
