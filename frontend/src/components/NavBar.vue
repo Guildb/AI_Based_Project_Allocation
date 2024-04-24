c11a0c3079ab3cc51a5e718b2ffdaf8d2ad86991
guildb <5silvr53@solent.ac.uk>
2024-04-24 23:55:11 +0100

    debugging

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,6 +5,15 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
+      <router-link
+        to="/dashboard"
+        class="flex items-center space-x-3 rtl:space-x-reverse"
+      >
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >Project Allocation</span
+        >
+      </router-link>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button
           @click="toggleMenu"
@@ -35,7 +44,7 @@
         :class="[
           !isMenuOpen ? 'hidden' : 'flex',
           'items-center',
-          'justify-center',
+          'justify-between',
           'w-full',
           'md:flex',
           'md:w-auto',
@@ -44,7 +53,7 @@
         id="navbar-sticky"
       >
         <ul
-          class="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:mx-auto md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
             <router-link
