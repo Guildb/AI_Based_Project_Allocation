52d9ee50ccc20c5b460ed09597cf60bf64dfda96
guildb <5silvr53@solent.ac.uk>
2024-04-24 23:53:10 +0100

    styling navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -35,7 +35,7 @@
         :class="[
           !isMenuOpen ? 'hidden' : 'flex',
           'items-center',
-          'justify-between',
+          'justify-center',
           'w-full',
           'md:flex',
           'md:w-auto',
@@ -44,7 +44,7 @@
         id="navbar-sticky"
       >
         <ul
-          class="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:mx-auto md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
             <router-link
