b1704453ba1b92bb36f8d781b56e34c55bab7ca4
guildb <5silvr53@solent.ac.uk>
2024-04-29 14:32:56 +0100

    styling changes

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,27 +1,33 @@
 <template>
-  <div class="min-h-screen flex justify-center items-center p-24">
+  <div class="min-h-screen flex justify-center items-center p-4 sm:p-24">
     <div
-      class="w-3/4 bg-gray-200 bg-opacity-50 rounded-lg shadow-lg p-4 transition-opacity duration-700 ease-in opacity-0"
+      class="w-full sm:w-3/4 bg-gray-200 bg-opacity-50 rounded-lg shadow-lg p-4 transition-opacity duration-700 ease-in opacity-0"
       :class="{ 'opacity-100': isAnimated }"
     >
-      <h1 class="text-4xl font-bold mb-4 text-slate-700">Welcome to my page</h1>
-      <p class="text-lg mb-6 text-slate-700">
+      <h1
+        class="text-2xl sm:text-4xl font-bold mb-4 text-center text-slate-700"
+      >
+        Welcome to my page
+      </h1>
+      <p class="text-base sm:text-lg mb-6 text-center text-slate-700">
         This website is an AI-based project allocation, you will need to have an
         account in order to use this service.
       </p>
-      <p class="text-lg mb-6 text-slate-700">
+      <p class="text-base sm:text-lg mb-6 text-center text-slate-700">
         If you do not have a project idea yet you will be able to see the
-        projects proposed by the tutors
+        projects proposed by the tutors.
       </p>
-      <div class="space-x-4 mt-6">
+      <div
+        class="text-center flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mt-6"
+      >
         <router-link
           to="/login"
-          class="bg-slate-700 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          class="bg-slate-700 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-block mx-2 sm:w-1/2"
           >Sign in</router-link
         >
         <router-link
           to="/signup"
-          class="bg-slate-700 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          class="bg-slate-700 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-block mx-2 sm:w-1/2"
           >Create Account</router-link
         >
       </div>
